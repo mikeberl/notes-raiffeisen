92db0c4451acdb9ad8ad390ceeeb705697af82cc
mikeberl <berlanda94@gmail.com>
2025-01-08 18:10:57 +0100

    Frontend - Accordion Bugfix when page is loaded

--- frontend/src/app/components/notes-list/notes-list.component.ts
+++ frontend/src/app/components/notes-list/notes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Note } from '../../models/note.model';
 import { NoteService } from '../../services/note.service';
 
@@ -7,22 +7,29 @@ import { NoteService } from '../../services/note.service';
   templateUrl: './notes-list.component.html',
   styleUrl: './notes-list.component.scss'
 })
+
 export class NotesListComponent implements OnInit {
 
   notes: Note[] = [];
   filteredNotes: Note[] = [];
   searchTitle: string = '';
 
+  collapsedNotes: { [key: number]: boolean } = {};
+
   constructor(private noteService: NoteService) {}
 
   ngOnInit(): void {
-    this.loadNotes();
+    this.loadNotes();  
   }
 
   loadNotes(): void {
     this.noteService.getAllNotes().subscribe((notes) => {
       this.notes = notes;
       this.filteredNotes = notes;
+
+      this.filteredNotes.forEach(note => {
+        this.collapsedNotes[note.id] = true;
+      });
     });
   }
 
@@ -46,5 +53,9 @@ export class NotesListComponent implements OnInit {
     this.filteredNotes = this.notes.filter((note) =>
       note.title.toLowerCase().includes(this.searchTitle.toLowerCase())
     );
+  }
+
+  toggleAccordion(noteId: number) {
+    this.collapsedNotes[noteId] = !this.collapsedNotes[noteId];
   }
 }
